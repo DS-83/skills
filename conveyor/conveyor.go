faf47fb7dc3bacd6150840bb168a2c93cf859e5b
DS-83 <63358079+DS-83@users.noreply.github.com>
2022-12-01 12:27:06 +0300

    Revert "solution"

--- conveyor/conveyor.go
+++ conveyor/conveyor.go
@@ -1,28 +1,5 @@
 package conveyor
 
-import (
-	"sync"
-)
-
 func RunConveyor(tasksFlow ...task) {
-	in := make(chan interface{})
-
-	var wg sync.WaitGroup
-
-	for _, task := range tasksFlow {
-		out := make(chan interface{})
-
-		wg.Add(1)
-		go func(task func(in, out chan interface{}), in, out chan interface{}) {
-			task(in, out)
-			close(out)
-			wg.Done()
-
-		}(task, in, out)
-
-		in = out
-	}
-
-	wg.Wait()
-
+	// implement conveyor in here
 }
